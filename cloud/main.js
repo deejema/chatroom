220d874264e9fdd788767a2166a53dee4bb1e347
Derek <deejema@gmail.com>
2018-05-10 18:24:11 -0700

    client side parse

--- cloud/main.js
+++ cloud/main.js
@@ -8,10 +8,10 @@ Parse.Cloud.define('getLog', function(req, res) {
 	var query = new Parse.Query("chat");
 	query.find({
 		error: function(err) {
-			return response.error(err)
+			response.error(err)
 		},
 		success: function(res) {
-			return response.success(res)
+			response.success(res)
 		}
 	});
 });
